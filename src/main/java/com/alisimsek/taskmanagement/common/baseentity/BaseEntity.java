d769188ca846b07102b9f6815f2009c704b7aa0c
Ali <ali.simsek035@gmail.com>
2025-02-25 23:53:08 +0300

    Added base entity.

---
+++ src/main/java/com/alisimsek/taskmanagement/common/baseentity/BaseEntity.java
@@ -0,0 +1,58 @@
+package com.alisimsek.taskmanagement.common.baseentity;
+
+import com.alisimsek.taskmanagement.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String guid = UUID.randomUUID().toString();
+
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User createdBy;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User modifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+
+    @Enumerated(EnumType.STRING)
+    private EntityStatus entityStatus;
+
+    public void delete() {
+        if (EntityStatus.ACTIVE.equals(getEntityStatus())) {
+            this.entityStatus = EntityStatus.DELETED;
+        }
+    }
+
+    public void activate() {
+        if (EntityStatus.DELETED.equals(getEntityStatus())) {
+            this.entityStatus = EntityStatus.ACTIVE;
+        }
+    }
+}
