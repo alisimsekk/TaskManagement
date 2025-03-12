6d3415c5f4e4a48ff8de090bf58a0daa2fddfd10
Ali <ali.simsek035@gmail.com>
2025-03-12 22:17:21 +0300

    Updated BaseRepository

--- src/main/java/com/alisimsek/taskmanagement/common/base/BaseRepository.java
+++ src/main/java/com/alisimsek/taskmanagement/common/base/BaseRepository.java
@@ -34,12 +34,14 @@ public interface BaseRepository<T extends BaseEntity, ID extends Long> extends J
     default void activate(T entity) {
         if (EntityStatus.DELETED.equals(entity.getEntityStatus())) {
             entity.activate();
+            save(entity);
         }
     }
 
     default void delete(T entity) {
         if (EntityStatus.ACTIVE.equals(entity.getEntityStatus())) {
             entity.delete();
+            save(entity);
         }
     }
 
